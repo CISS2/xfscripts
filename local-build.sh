e02318e038cead43f465eecbc29b77fcc7848eab
Raul Dipeas <rauldipeas@mail.com>
2020-05-22 05:15:23 -0300

    ^.gitignore ^local-build ^gh-pages

--- local-build.sh
+++ local-build.sh
@@ -4,7 +4,6 @@ mdbook init --force xfscripts
 cp -varf src xfscripts/
 cp -varf book.toml xfscripts/
 rm .gitignore
-npx embedme xfscripts/src/scripts/apt.md
-npx embedme xfscripts/src/scripts/debs.md
+npx embedme xfscripts/src/scripts/apt.md xfscripts/src/scripts/debs.md
 echo xfscripts > .gitignore
 mdbook build -o xfscripts
